daad3a5bc4c2d9a671aec998ae9959148b0e200a
maridavanso <mariana.regalau@gmail.com>
2021-02-17 15:33:15 -0300

    casa-do-codigo

---
+++ src/main/java/br/com/apizup/casadocodigo/novoautor/NovoAutorController.java
@@ -0,0 +1,10 @@
+package br.com.apizup.casadocodigo.novoautor;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NovoAutorController {
+
+	
+	
+}
---
+++ src/test/java/br/com/apizup/casadocodigo/CasadocodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apizup.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadocodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
